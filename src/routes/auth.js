19508b9b97042bc37626b0916cb4ab4ffa47b5be
ahmad dhohir azhari <dhohir12@gmail.com>
2022-10-20 02:01:49 +0700

    finish auth module without nodemiler

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,6 +5,11 @@ const Router = express.Router();
 const authController = require("../controllers/auth");
 
 Router.post("/register", authController.register);
-Router.get("/user", authController.getAllUser);
+Router.get("/verify", authController.verify);
+Router.post("/login", authController.login);
+Router.post("/logout", authController.logout);
+Router.post("/refresh", authController.refresh);
+Router.post("/forgotPassword", authController.forgotPassword);
+Router.patch("/resetPassword/:OTPReset", authController.resetPassword);
 
 module.exports = Router;
