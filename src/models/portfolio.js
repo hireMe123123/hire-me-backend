599bc587e48f6fead0b599a8ff0c1bc3b840d3f6
ahmad dhohir azhari <dhohir12@gmail.com>
2022-10-20 23:52:29 +0700

    finish module portfolio

---
+++ src/models/portfolio.js
@@ -0,0 +1,73 @@
+const supabase = require("../config/supabase");
+
+module.exports = {
+  createPortfolio: (data) =>
+    new Promise((resolve, reject) => {
+      supabase
+        .from("portofolio")
+        .insert([data])
+        .then((result) => {
+          if (!result.error) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+    }),
+  getPortfolioByUserId: (userId) =>
+    new Promise((resolve, reject) => {
+      supabase
+        .from("portofolio")
+        .select("*")
+        .eq("userId", userId)
+        .then((result) => {
+          if (!result.error) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+    }),
+  getPortfolioById: (portofolioId) =>
+    new Promise((resolve, reject) => {
+      supabase
+        .from("portofolio")
+        .select("*")
+        .eq("portofolioId", portofolioId)
+        .then((result) => {
+          if (!result.error) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+    }),
+  deletePortfolio: (id) =>
+    new Promise((resolve, reject) => {
+      supabase
+        .from("portofolio")
+        .delete()
+        .eq("portofolioId", id)
+        .then((result) => {
+          if (!result.error) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+    }),
+  updatePortfolio: (id, data) =>
+    new Promise((resolve, reject) => {
+      supabase
+        .from("portofolio")
+        .update(data)
+        .eq("portofolioId", id)
+        .then((result) => {
+          if (!result.error) {
+            resolve(result);
+          } else {
+            reject(result);
+          }
+        });
+    }),
+};
