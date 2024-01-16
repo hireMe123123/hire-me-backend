7ba754b148015c110b216afa31721a7bd5b70c41
hireMe123123 <116066502+hireMe123123@users.noreply.github.com>
2024-01-16 07:07:46 +0700

    fix redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,23 +1,23 @@
-const redis = require("redis");
+// const redis = require("redis");
 
-const redisPassword = process.env.REDIS_PASWORD;
-const redisHost = process.env.REDIS_HOST;
-const redistPort = process.env.REDIS_PORT;
+// const redisPassword = process.env.REDIS_PASWORD;
+// const redisHost = process.env.REDIS_HOST;
+// const redistPort = process.env.REDIS_PORT;
 
-const client = redis.createClient({
-  socket: {
-    host: redisHost,
-    port: redistPort,
-  },
-  password: redisPassword,
-});
+// const client = redis.createClient({
+//   socket: {
+//     host: redisHost,
+//     port: redistPort,
+//   },
+//   password: redisPassword,
+// });
 
-(async () => {
-  client.connect();
-  client.on("connect", () => {
-    // eslint-disable-next-line no-console
-    console.log("You're connected db redis ...");
-  });
-})();
+// (async () => {
+//   client.connect();
+//   client.on("connect", () => {
+//     // eslint-disable-next-line no-console
+//     console.log("You're connected db redis ...");
+//   });
+// })();
 
-module.exports = client;
+// module.exports = client;
